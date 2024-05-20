8658bac20026b370f7438f9fa68168603c774f10
Droidna405 <joemwangala@gmail.com>
2024-05-20 05:46:29 +0300

    PEP8

--- console.py
+++ console.py
@@ -7,8 +7,13 @@ import json
 
 
 class AirbnbCLI(cmd.Cmd):
-    """Command interpreter for Airbnb."""
-
+    """
+    Command interpreter for Airbnb.
+    Attributes:
+        prompt (str): The command prompt string.
+        user_db_file (str): File path for storing user data.
+        place_db_file (str): File path for storing place data.
+    """
     prompt = "(hbnb) "
     user_db_file = 'users.json'
     place_db_file = 'places.json'
