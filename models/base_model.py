f20f0a6809d305fff26b10ac8b5366a46fa8996a
Droidna405 <joemwangala@gmail.com>
2024-05-21 21:54:19 +0300

    basemodel from dict

--- models/base_model.py
+++ models/base_model.py
@@ -23,10 +23,24 @@ class BaseModel:
         to_dict(self): Returns a dictionary representation of the instance.
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self,  *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    # Skip the "__class__" key
+                    setattr(self, key, value)
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            else:
+                # Convert created_at from string to datetime
+                self.created_at = datetime.strptime
+                (kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.id = kwargs.get("id", str(uuid.uuid4()))
+            self.updated_at = kwargs.get("updated_at", datetime.now())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return f"[BaseModel] ({self.id}) {self.__dict__}"
